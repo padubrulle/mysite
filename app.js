7f7097d5401853c640add7d888b3eb963efeea17
padub <pierrealain.dubrulle@gmail.com>
2024-11-03 14:29:58 +0100

    starting nodejs app from scratch with a watcher. Will be replaced later by react, express or vue.

---
+++ app.js
@@ -0,0 +1,11 @@
+import { createServer } from "node:http";
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World");
+});
+
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000/");
+})
